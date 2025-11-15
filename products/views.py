983ca56434c19ac990b6210b9103d45f3808b8e6
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-15 15:58:54 +0100

    add products view and urls - add product list basic test template - add header to the producs tempalte

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
 
 # Create your views here.
+
+
+def all_products(request):
+    """Show all products"""
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products/products.html", context)
+
+
+def product_detail(request, product_id):
+    """Show individual product details"""
+    product = get_object_or_404(Product, pk=product_id)
+    context = {"product": product}
+    return render(request, "products/product_detail.html", context)

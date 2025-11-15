e01f75efcbd164355e0ce885df5f5d59ec758841
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-15 15:47:43 +0100

    Revert "add products view and urls"
    This reverts commit 2bf467cbdc650a43280870a42a60193942b478be.

--- products/views.py
+++ products/views.py
@@ -1,18 +1,3 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render
 
-# Create your views here
-
-
-def all_products(request):
-    """Show all products"""
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "products/products.html", context)
-
-
-def product_detail(request, product_id):
-    """Show individual product details"""
-    product = get_object_or_404(Product, pk=product_id)
-    context = {"product": product}
-    return render(request, "products/product_detail.html", context)
+# Create your views here.

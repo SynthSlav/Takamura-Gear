f6afad29e207592bca2d5960cf744adf640816fe
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-16 18:49:59 +0100

    add cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,6 +4,7 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get("cart", {})
 
     context = {
         "cart_items": cart_items,
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
+
+
+def view_cart(request):
+    """Display the shopping cart"""
+    return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, product_id):
+    """Add a product to the shopping cart"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    size = None
+
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+
+    cart = request.session.get("cart", {})
+
+    if size:
+        # Product has size
+        if product_id in list(cart.keys()):
+            cart[product_id]["quantity"] += quantity
+        else:
+            cart[product_id] = {"quantity": quantity, "size": size}
+    else:
+        # No size
+        if product_id in list(cart.keys()):
+            cart[product_id] += quantity
+        else:
+            cart[product_id] = quantity
+
+    request.session["cart"] = cart
+    messages.success(request, f"Added {product.name} to your cart")
+    return redirect(redirect_url)

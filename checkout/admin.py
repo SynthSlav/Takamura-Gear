79b2bf542f2b58d5fa9d0a308a1a09f9b23c25e1
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-18 13:45:15 +0100

    register order model in the admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin interface for Order model
+    """
+
+    list_display = (
+        "order_number",
+        "date",
+        "full_name",
+        "email",
+    )
+
+    ordering = ("-date",)
+
+
+admin.site.register(Order, OrderAdmin)

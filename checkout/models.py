f300d7069de9aa836d509e47adcd7992553e9d25
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-18 14:15:30 +0100

    add order line items model for individual order items

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from products.models import Product
 
 # Create your models here.
 
@@ -49,3 +50,36 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_number
+
+
+class OrderLineItem(models.Model):
+    """
+    Individual item in an order
+    Represents one product (with optional size) and quantity
+    """
+
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name="lineitems",
+    )
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE
+    )
+    product_size = models.CharField(max_length=10, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        """
+        Override save to calculate lineitem total
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"SKU {self.product.sku} on order {self.order.order_number}"

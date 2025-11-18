0e443cc0304afe4769ef0e60cd97c9d976d8f24d
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-19 00:05:37 +0100

    rectify mistaken file for apps, - add orderform model for checkout

--- cart/apps.py
+++ cart/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
 
-class CheckoutConfig(AppConfig):
+class CartConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "checkout"
-
-    def ready(self):
-        """Import signals when app is ready"""
-        import checkout.signals
+    name = "cart"
--- checkout/apps.py
+++ checkout/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkout'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "checkout"
+
+    def ready(self):
+        """Import signals when app is ready"""
+        import checkout.signals

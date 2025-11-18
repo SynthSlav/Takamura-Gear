643e7b0839f8112c8b4b18cf3b8031ed8e2be0a1
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-18 13:04:55 +0100

    add basic order model with auto generated order numbers

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,29 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    """
+    Store customer order information
+    """
+
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def _generate_order_number(self):
+        """Generate random unique order number using UUID"""
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """Override save to set order number if not already set"""
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number

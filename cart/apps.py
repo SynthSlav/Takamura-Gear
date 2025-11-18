228895ddb03f011ec56f26b45f1e26e9cc52df80
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-18 23:54:20 +0100

    connect order update signals in checkoutconfig

--- cart/apps.py
+++ cart/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
-class CartConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cart'
+class CheckoutConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "checkout"
+
+    def ready(self):
+        """Import signals when app is ready"""
+        import checkout.signals

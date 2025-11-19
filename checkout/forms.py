23bbf49d661dbc0c6ae117a1d99b48e2911c0165
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-19 01:19:39 +0100

    Add placeholders and styling to OrderForm fields, - add custom placeholders with required field indicators

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,3 +20,32 @@ class OrderForm(forms.ModelForm):
             "country",
             "county",
         )
+
+
+def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    placeholders = {
+        "full_name": "Full Name",
+        "email": "Email Address",
+        "phone_number": "Phone Number",
+        "street_address1": "Street Address 1",
+        "street_address2": "Street Address 2",
+        "town_or_city": "Town or City",
+        "county": "County, State or Locality",
+        "postcode": "Postal Code",
+        "country": "Country",
+    }
+
+    self.fields["full_name"].widget.attrs["autofocus"] = True
+
+    for field in self.fields:
+        # Add asterisk to required fields
+        if self.fields[field].required:
+            placeholder = f"{placeholders[field]} *"
+        else:
+            placeholder = placeholders[field]
+
+        self.fields[field].widget.attrs["placeholder"] = placeholder
+        self.fields[field].widget.attrs["class"] = "form-control"
+        self.fields[field].label = False  # Using placeholders instead

6929e55443879512d1c1dd92397888a20a9bb949
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-21 19:16:06 +0100

    register userpfrofile in admin pannel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin interface for UserProfile model
+    """
+
+    list_display = (
+        "user",
+        "default_phone_number",
+        "default_town_or_city",
+        "default_country",
+    )
+
+    ordering = ("user",)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)

e339dc3e93ff95a81d2e62b25f3d7999cc8edfa4
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-21 22:56:23 +0100

    create profile view for displaying and updating user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
+from checkout.models import Order
 
 # Create your views here.
+
+
+@login_required
+def profile(request):
+    """
+    Display the user's profile
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully")
+        else:
+            messages.error(request, "Update failed. Please check the form.")
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
+    template = "profiles/profile.html"
+    context = {
+        "form": form,
+        "orders": orders,
+    }
+
+    return render(request, template, context)

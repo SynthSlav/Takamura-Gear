2bf212fd49e5cde8799d85c747c458f4df626be6
SynthSlav <ljupcebozinovski@gmail.com>
2025-11-22 09:28:59 +0100

    Add default full name field to user profile and pre-fill checkout

--- checkout/views.py
+++ checkout/views.py
@@ -94,7 +94,27 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        # Pre-fill form with profile info if user is logged in
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(
+                    initial={
+                        "full_name": profile.default_full_name,
+                        "email": request.user.email,
+                        "phone_number": profile.default_phone_number,
+                        "street_address1": profile.default_street_address1,
+                        "street_address2": profile.default_street_address2,
+                        "town_or_city": profile.default_town_or_city,
+                        "county": profile.default_county,
+                        "postcode": profile.default_postcode,
+                        "country": profile.default_country,
+                    }
+                )
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     template = "checkout/checkout.html"
     context = {
